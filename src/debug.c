2e1eaa269d2f6111ec17fe24da7b991fde3c530a
MarkusJx <28785953+markusjx@users.noreply.github.com>
2021-12-25 11:02:32 +0100

    Try to fix the build (2)
    Signed-off-by: Markus <28785953+MarkusJx@users.noreply.github.com>

--- src/debug.c
+++ src/debug.c
@@ -1,10 +1,10 @@
 #include "pusha/helper.h"
 
 #ifdef PUSHA_WINDOWS
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
-
-#pragma comment(lib, "Ws2_32.lib")
 #endif //PUSHA_WINDOWS
 
 #include "pusha/debug.h"
